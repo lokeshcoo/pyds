66a435abe00ef76cdb0a3e67eccc2c19fa2f5f53
Lokesh Mishra <lokeshmisra2813@gmail.comC:/Users/ASUS/miniconda3/Scripts/activate.bat>
2023-08-22 10:51:05 +0530

    turtle2

---
+++ patterna.py
@@ -0,0 +1,25 @@
+from turtle import *
+size = 100
+width=50
+pencolor('black')
+speed('slow')
+fillcolor=('yellow')
+begin_fill()
+left(60)
+forward(250)
+right(120) 
+forward(250)
+left(180)
+penup()
+forward(125)
+pendown()
+left(60)
+forward(125)
+hideturtle()
+
+
+
+
+
+
+mainloop()
---
+++ patternsq.py
@@ -0,0 +1,14 @@
+from turtle import*  
+size = 100
+width=50
+pencolor('black')
+speed('slow')
+left(90)
+forward(180)
+right(90)
+forward(180)
+right(90)
+forward(180)
+right(90)
+forward(180)
+mainloop()
